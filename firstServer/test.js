2e2b303dfc08f164e739141608a690a953055b58
HongMoonGI <hmoongi94@gmail.com>
2023-10-04 12:09:59 +0900

    서버가동

--- firstServer/test.js
+++ firstServer/test.js
@@ -1,1 +1,14 @@
-console.log("hello World")
+const http = require('http');
+
+http.createServer(function(request, response){
+  let writeHeadObject = {
+    'content - Type': 'text/html'
+  }
+
+  response.writeHead(200, writeHeadObject);
+  response.end("hello");
+
+
+}).listen(8080)
+
+// 서버 가동 후 끄는 법 -> ctrl + c
