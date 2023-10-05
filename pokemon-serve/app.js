ad6def6f37b227ba897d8fad3c48764f36719c2c
HongMoonGI <hmoongi94@gmail.com>
2023-10-05 12:24:54 +0900

    서버 연습

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const http = require("http");
+
+const server = http.createServer(function(request,response){
+  // console.log(request)
+  // console.log(response)
+  // console.log(typeof(response))
+
+  response.writeHead(200, {"Content-Type": "text/html"})
+
+  let doc = `<html> <head> </head> <body> <h1> hello zddz</h1> </body> </html>`
+  response.end(doc)
+});
+
+server.listen(1234)
