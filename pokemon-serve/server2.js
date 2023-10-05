72ff280cc8f01cf23b0a0ef54343ef2519374a99
HongMoonGI <hmoongi94@gmail.com>
2023-10-05 14:59:15 +0900

    서버 연습

--- pokemon-serve/server2.js
+++ pokemon-serve/server2.js
@@ -7,12 +7,13 @@ const server = http.createServer(function(request,response){
 
   console.log(request.url)
   if(request.url === "/"){
-    let doc = "<html><head><body><h1>hello world 하이월드</h1></body></head></html>"
+    let doc = `<html><head><body><h1>hello world 하이월드</h1><a href ="https://localhost:1234/sub">서브로 이동하기</a></body></head></html>`
+
     response.writeHead(200, "utf-8", {"Content-Type":"text/html"})
     response.end(doc)
   }
   if(request.url === "/sub"){
-    let doc2 = "<html><head><body><h1>bye world 바이월드</h1></body></head></html>"
+    let doc2 = `<html><head><body><h1>bye world 바이월드</h1><a href = "https://localhost:1234/">메인서버로 이동하기</a></body></head></html>`
     response.writeHead(200, "utf-8", {"Content-Type":"text/html"})
     response.end(doc2)
   }
