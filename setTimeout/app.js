527564e4b72d8f0cff13f00113ff5cbf043c5b75
HongMoonGI <hmoongi94@gmail.com>
2023-10-06 10:48:51 +0900

    setTimeout 수업

--- setTimeout/app.js
+++ setTimeout/app.js
@@ -1,3 +1,13 @@
 setTimeout(function(){
-  console.log("test")
+  console.log("첫번째 소사")
+
+  setTimeout(function(){
+    console.log("두번째 뭉기")
+
+    setTimeout(function(){
+      console.log("세번째 밍구")
+    },1000)
+  },2000)
 },3000)
+
+
