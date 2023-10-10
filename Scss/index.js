2a49f9f8636424f2705c234fa9f2fb4f80a96089
HongMoonGI <hmoongi94@gmail.com>
2023-10-10 11:58:31 +0900

    포케몬리스트 파일만들어보기

--- Scss/index.js
+++ Scss/index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const pokemon = require('pokemon')
+console.log(pokemon.all('ko'))
+
+// write로 html 파일 만들고 포케몬 이름 리스트로 제작해보기
+
+const fs = require('fs');
+fs.writeFile("class/Scss/pokemonList.html", pokemon, err =>{
+  if(err){
+    console.error(err);
+  }
+  // file written successfully
+})
