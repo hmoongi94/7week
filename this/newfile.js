ad4dc420a5c8f55ed9220ea6d421dd9a95392d5f
HongMoonGI <hmoongi94@gmail.com>
2023-10-11 11:28:48 +0900

    this로 객체만들어보기

--- this/newfile.js
+++ this/newfile.js
@@ -1,4 +1,4 @@
-function student(name, number){
+function Student(name, number){
   this.name = name
   this.number = number
 }
@@ -8,11 +8,14 @@ function student(name, number){
 /////////////////////////////////////////
 
 
+
 let list = ["홍문기", "김현"]
 let studentArr = [];
 
 for(let i=0; i<list.length; i++){
-  studentArr[i] = new student(list[i],i+1)
+  studentArr[i] = new Student(list[i],i+1)
 }
 
 console.log(studentArr)
+
+// 생성자 함수(거푸집)은 첫글자를 대문자로 쓰는 관습이있다.
