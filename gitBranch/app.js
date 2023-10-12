b92b35eeb8c7fc4cb2c053de435b06235c74d4ea
HongMoonGI <hmoongi94@gmail.com>
2023-10-12 10:57:09 +0900

    향마루 응답처리

--- gitBranch/app.js
+++ gitBranch/app.js
@@ -1,7 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-
+  res.writeHead(200,"utf-8",{"Content-Type" : "text/plain"})
+  // plain -> 텍스트만 던짐
+  // html -> html로 만듬
+  res.write("향마루");
+  res.end();
 });
 
 server.listen(3500,(err)=>{
@@ -9,5 +13,6 @@ server.listen(3500,(err)=>{
     console.error("서버 에러남")
   } else{
     console.log("3500포트에 돌아감 http://localhost:3500");
+    // 꿀팁) 로그에 주소적으면 콘솔에서 클릭해서 서버들어갈 수 있음.
   }
 })
