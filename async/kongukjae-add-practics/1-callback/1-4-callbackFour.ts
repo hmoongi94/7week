95eb018a17b3ef33d971a694b428f4d6bcb56803
HongMoonGI <hmoongi94@gmail.com>
2024-01-20 13:06:33 +0900

    feat: 인터페이스 적용해보기

--- async/kongukjae-add-practics/1-callback/1-4-callbackFour.ts
+++ async/kongukjae-add-practics/1-callback/1-4-callbackFour.ts
@@ -1,6 +1,19 @@
-function callbackFour(a: number, b: number, multiplyCallback: (result: number) => void) {
-  let sum = a + b;
-  multiplyCallback(sum);
+interface callbackFour {
+  (a: number, b: number, multiplyCallback: (result: number) => void): void;
+}
+
+// function callbackFour(
+//   a: number,
+//   b: number,
+//   multiplyCallback: (result: number) => void
+// ) {
+//   let sum = a + b;
+//   multiplyCallback(sum);
+// }
+
+function callbackFour(a,b,multiplyCallback){
+  let sum = a+ b;
+  multiplyCallback(sum)
 }
 
 callbackFour(5, 5, (sumResult) => {
